9b1857e90b33527479703cc137c1de619edd80bc
Alexhuszagh <ahuszagh@gmail.com>
2016-10-20 18:31:58 -0500

    Updated signature for IConvConst

--- IConvConst.cmake
+++ IConvConst.cmake
@@ -4,34 +4,27 @@
 # Check if IConv's first parameter takes constant argument.
 #
 # IConv implementations support two function signatures:
-#   iconv(iconv_t cd, char **pIn, size_t *srclen, char **pIn, size_t *dstlen);
-#   iconv(iconv_t cd, char **pIn, size_t *srclen, const char **pIn, size_t *dstlen);
+#   iconv(iconv_t cd, char **pIn, size_t *srclen, char **pOut, size_t *dstlen);
+#   iconv(iconv_t cd, const char **pIn, size_t *srclen, char **pOut, size_t *dstlen);
 #
 # Sets `ICONV_CONST` if iconv takes a constant argument.
 
-include(CheckCSourceCompiles)
+include(CheckPrototypeDefinition)
 
-# CHECK
-# -----
-
-if(NOT IConv_FOUND)
-    message(SEND_ERROR "Cannot try to compile Iconv without the library")
-endif()
+# PROTOTYPE
+# ---------
 
 list(APPEND CMAKE_REQUIRED_INCLUDES ${IConv_INCLUDE_DIRS})
 
-check_source_compiles("
-#include <stdlib.h>
-#include <iconv.h>
+check_prototype_definition(iconv
+    "size_t iconv(iconv_t cd, const char **pIn, size_t *srclen, char **pOut, size_t *dstlen)"
+    "NULL"
+    "stdlib.h;iconv.h"
+    ICONV_CONST
+)
 
-int main()
-{
-    iconv_t conv = iconv_open(0, 0);
-    const char *pIn;
-    char *pOut;
-    size_t srclen, dstlen;
-    iconv(conv, &pIn, &srclen, &pOut, &dstlen);
-    iconv_close(conv);
-    return 0;
-}
-" ICONV_CONST)
+if(ICONV_CONST)
+    add_definitions(-DICONV_CONST=const)
+else()
+    add_definitions(-DICONV_CONST)
+endif()
